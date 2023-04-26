1e5892e540e5c35e7f78e10669f0178e71dfa0c5
Mruzik1 <mimisha2014@gmail.com>
2023-04-26 09:55:34 +0200

    added modified versions of the best model (4), experimented with those

--- nn_architectures/__init__.py
+++ nn_architectures/__init__.py
@@ -3,3 +3,6 @@ from .model2 import Model2
 from .model3 import Model3
 from .model4 import Model4
 from .model5 import Model5
+from .model4_1 import Model4_1
+from .model4_2 import Model4_2
+from .model4_3 import Model4_3
